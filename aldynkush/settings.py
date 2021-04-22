fb06983738dc283a92ffaacb9a75ff7ad5d375a8
Aldynkushkash <aldyn-kushkash@yandex.ru>
2021-04-22 16:53:44 +0300

    added email settings

--- aldynkush/settings.py
+++ aldynkush/settings.py
@@ -138,6 +138,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'home.AdvUser'
 
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = '465'
+EMAIL_HOST_USER = 'admin@aldynkushkash.ru'
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = '10011979AKya'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
